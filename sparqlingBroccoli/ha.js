299be5ee70a18119eee1165c5d9a1622c9c22015
Marco Koch <marco@5th.ch>
2019-06-24 14:03:31 +0100

    generating N-Triples form github data.

--- sparqlingBroccoli/ha.js
+++ sparqlingBroccoli/ha.js
@@ -1,8 +1,11 @@
 gql = require('graphql-request')
 const rdf = require('rdf');
-const { NamesNode, BlankNode, Literal } = rdf;
+const { NamedNode, BlankNode, Literal } = rdf;
 
-const foaf = rdf.ns('http://xmlns.com/foaf/0.1/');
+const foaf = new rdf.ns('http://xmlns.com/foaf/0.1/');
+const doap = new rdf.ns('http://usefulinc.com/ns/doap#');
+const succotash = new rdf.ns('http://5th.ch/succotash/');
+const me = new NamedNode('http://marcoko.ch/#i');
 
 const query = `
 query {
@@ -24,16 +27,90 @@ query {
 headers = { authorization: 'bearer ' + process.env['GITHUB_TOKEN'] }
 const client = new gql.GraphQLClient('https://api.github.com/graphql', {headers: headers});
 
-var Graph = new rdf.Graph();
+var gBroccoli = new rdf.Graph();
+var newTriples = [];
 
 
 client.request(query)
 .then(data => {
-    data.viewer.allRepos.edges.forEach(r => {
-        console.log(new rdf.Triple(
-                new BlankNode(),
-                rdf.rdfsns('label'),
-                new Literal(r.node.name, '@en')).toNT()
-        ); 
+    data.viewer.allRepos.edges.forEach(function blahh(r) {
+
+        node = succotash('#' + r.node.name)
+
+        newTriples.push(
+            new rdf.Triple(
+                node,
+                doap('name'),
+                new Literal(r.node.name, '@en')
+            )
+        );
+
+        newTriples.push(
+            new rdf.Triple(
+                node,
+                rdf.rdfsns('type'),
+                doap('Project')
+            )
+        );
+
+
+
+
+        homepage = r.node.homepageUrl;
+        if (homepage) {
+            try {
+                newTriples.push(
+                    new rdf.Triple(
+                        node,
+                        doap('homepage'),
+                        new NamedNode(homepage)
+                    )
+                );
+            } catch(error) { console.log("failed to parse homepage.", r.node.name, homepage, error); }
+        }
+
+        description = r.node.description;
+        if (description) {
+            try {
+                newTriples.push(
+                    new rdf.Triple(
+                        node,
+                        doap('description'),
+                        new Literal(description, '@en')
+                    )
+                );
+            } catch(error) { console.log("failed to parse decription", r.node.name, description, error); }
+        }
+
+        newTriples.push(
+            new rdf.Triple(
+                node,
+                doap('repository'),
+                new NamedNode(r.node.url)
+            )
+        );
+
+
+        newTriples.push(
+            new rdf.Triple(
+                node,
+                foaf('maker'),
+                me
+            )
+        );
+
+
+
     })
+    
+    newTriplesAsString = '';
+
+    newTriples.forEach(t => {
+        newTriplesAsString += t.toNT();
+        console.log(t.toNT());
+    });
+
+
+
+
 });
