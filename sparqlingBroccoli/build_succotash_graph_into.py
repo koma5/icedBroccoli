d30aedb22f28a11e7e8d57b5c276cdfa3c998b3f
Marco Koch <marco@5th.ch>
2019-06-24 21:10:43 +0100

    building graph succotash from dbpedia sparql query.

--- sparqlingBroccoli/build_succotash_graph_into.py
+++ sparqlingBroccoli/build_succotash_graph_into.py
@@ -10,27 +10,55 @@ me = rdflib.URIRef("http://marcoko.ch/#i")
 
 graph.parse('./succotash_seed.ttl', format="turtle") # maybe get it from github or somewhere else
 
-resourcesToFetch = []
+resources_to_query = []
 
 for a in graph.triples((None, doap['programming-language'], None)):
-    resourcesToFetch.append(a[2])
-    #graph.parse(a[2])
+    resources_to_query.append(a[2])
 
 
 for a in graph.triples((None, dbo['computingPlatform'], None)):
-    resourcesToFetch.append(a[2])
-    #graph.parse(a[2])
+    resources_to_query.append(a[2])
 
 
+resources_to_query = set(resources_to_query)
+resources_to_query = [s.n3() for s in resources_to_query]
+sparql_values = ' '.join(resources_to_query)
 
-print(set(resourcesToFetch))
-"""
-ttl_to_post = graph.serialize(format="turtle")
+sparql_query = """
+PREFIX dbr: <http://dbpedia.org/resource/>
+PREFIX dbo: <http://dbpedia.org/ontology/>
+
+CONSTRUCT {{
+
+    ?res dbo:abstract ?abs .
+    ?res rdfs:label ?label .
+
+}}
+WHERE {{
+
+    ?res dbo:abstract ?abs .
+    ?res rdfs:label ?label .
+
+    VALUES ?res {{
+        {0}
+    }}
+}}
+""".format(sparql_values)
+
+params = {
+    'query': sparql_query,
+    'format': 'text/turtle'
+}
+
+r = requests.get('https://dbpedia.org/sparql', params)
+
+print(r.text)
+
+
+ttl_to_post = r.text
 
 headers = {
     'content-type': 'text/turtle',        
 }
 
 r = requests.post(sys.argv[1], headers=headers, data=ttl_to_post)
-
-"""
