7efd5872b07538680e6ca1eeacbe538f1599c351
Marco Koch <marco@5th.ch>
2019-07-05 10:12:08 +0100

    renamed hotShit into broccoli; renamed named graphs aaaand default graph is union.

---
+++ seedingbroccoli/build_broccoli_named_graphs.sh
@@ -0,0 +1,11 @@
+
+source secret.txt
+
+node code_broccoli_put.js \
+    http://icedbroccoli.5th.ch/broccoli#code
+
+python succotash_broccoli_put.py \
+    http://icedbroccoli.5th.ch/broccoli#succotash
+
+python koma5_broccoli_put.py \
+    http://icedbroccoli.5th.ch/broccoli#koma5
